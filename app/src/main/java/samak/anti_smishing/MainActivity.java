60334679e33dcfa6b259376fcec0411b23139bd6
LeAriCorN <pandorasb0x@naver.com>
2017-12-01 20:08:15 +0900

    1

---
+++ app/src/main/java/samak/anti_smishing/MainActivity.java
@@ -0,0 +1,32 @@
+package samak.anti_smishing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.TabActivity;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("검사하기") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("설정") ;
+        tabHost1.addTab(ts2) ;
+
+    }
+}
